4d983e56b698c5ce964207c34e0984857a41bb68
Yesha Modi <modiyesha196@gmail.com>
2026-07-19 00:29:17 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,6 @@
+print("Hello, Data Science!")
+
+print("My name is Yesha Modi.")
+print("I am a Data Science Intern at Codomax Digital Solutions.")
+print("My development environment is ready.")
+print("Day 1 of my internship is completed successfully.")
